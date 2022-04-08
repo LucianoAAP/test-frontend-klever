25e96300bd1189c70138442587060d441e611b65
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-04-08 18:21:52 -0300

    FIX: fixes conflicts

--- src/pages/AddToken.jsx
+++ src/pages/AddToken.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Header } from '../components';
-=======
 import { Header, AddTokenForm } from '../components';
->>>>>>> 424983bd5db957aad4053a6272e919ddcf7d8a45
 
 const AddToken = () => (
   <div>
     <Header />
-<<<<<<< HEAD
-=======
     <AddTokenForm />
->>>>>>> 424983bd5db957aad4053a6272e919ddcf7d8a45
   </div>
 );
 
